141ac2e6ebd2ae953ab048d59c4b0dd6acf9ee0b
Praveen <praying.sharma@gmail.com>
2025-04-05 13:02:18 +0530

    Add

--- Main.java
+++ Main.java
@@ -1,18 +1,36 @@
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Stack;
 
 public class Main {
+    public static boolean isbalanced(String str) {
+        Stack<Character> stk = new Stack<>();
+        int i = 0;
+        while (i < str.length()) {
+            if (str.charAt(i) == '(') {
+                stk.push(str.charAt(i));
+            }
+            if (str.charAt(i) == ')') {
+                if(stk.isEmpty()){
+                    return true;
+                }
+                stk.pop();
+            }
+            i++;
 
-   
+        }
+
+        if (stk.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) {
-        Stack<Integer> stk=new Stack<>();
-
-        stk.push(10);
-        stk.push(20);
-        System.out.println(stk);
-
-       
+        String str = "(()))";
+        boolean ans = isbalanced(str);
+        System.out.println(ans);
 
     }
 }
