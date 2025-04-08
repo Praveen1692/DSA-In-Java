7bb46ef59cf1a02858b6c6b2ac3fb814124165e7
Praveen <praying.sharma@gmail.com>
2025-04-08 21:59:53 +0530

    Add

--- Main.java
+++ Main.java
@@ -5,21 +5,9 @@ import java.util.Stack;
 public class Main {
     // write a program to print all natural number from n to 1;
 
-    
-
-
-      
-
-
-    
-
-
-    
-
     public static void main(String[] args) {
 
-        int[] arr={10,50,40,30,20};
-        System.out.println(secondLargest(arr));
+        int[] arr = { 10, 50, 40, 30, 20 };
 
     }
 }
