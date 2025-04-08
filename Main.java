36757896b70eb2b58092793d6318d949f2c986e2
Praveen <praying.sharma@gmail.com>
2025-04-08 21:50:24 +0530

    insertion

--- Main.java
+++ Main.java
@@ -5,19 +5,18 @@ import java.util.Stack;
 public class Main {
     // write a program to print all natural number from n to 1;
 
-    static int fact(int n){
-        // base condition
-        if(n==0 || n==1){
-            return n;
-        }
-        return n*fact(n-1);
+    public static int secondLargest(int[] arr){
+        Arrays.sort(arr);
+        return arr[arr.length-2];
     }
 
+
     
+
     public static void main(String[] args) {
 
-        int n = 5;
-        System.out.println(fact(n));
+        int[] arr={10,50,40,30,20};
+        System.out.println(secondLargest(arr));
 
     }
 }
