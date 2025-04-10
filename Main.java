0380a4585eaaecd926aac55eafccb1d8f178bd55
Praveen <praying.sharma@gmail.com>
2025-04-10 21:04:14 +0530

    Add

--- Main.java
+++ Main.java
@@ -8,20 +8,29 @@ public class Main {
         if (b == 1) {
             return a;
         }
-        if(a==1){
+        if (a == 1) {
             return b;
         }
-        int result=power(a, b/2);
-        return result*result;
+        int result = power(a, b / 2);
+        return result * result;
     }
-    
-    
 
     public static void main(String[] args) {
 
-        int a = 2;
-        int b = 4;
-        System.out.println(power(a, b));
+        int[] arr = { 4, 5, 200, 25 };
+        int[] newarr = new int[arr.length];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    newarr[i] = arr[j];
+                    break;
+                }
+
+            }
+
+        }
+        System.out.println(Arrays.toString(newarr));
 
     }
 }
