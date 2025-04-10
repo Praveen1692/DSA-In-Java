a7d89941713465c85ea25b68fcecbc7c3ce07a0f
Praveen <praying.sharma@gmail.com>
2025-04-10 14:17:46 +0530

    Add

--- Main.java
+++ Main.java
@@ -3,11 +3,24 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class Main {
-    // write a program to print all natural number from n to 1;
+
+    public static int power(int a, int b) {
+        if (a == 1) {
+            return b;
+
+        }
+        if (b == 1) {
+            return a;
+        }
+        return a * power(a, b - 1);
+
+    }
 
     public static void main(String[] args) {
 
-        int[] arr = { 10, 50, 40, 30, 20 };
+        int a = 2;
+        int b = 3;
+        System.out.println(power(a, b));
 
     }
 }
