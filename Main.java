11a0618096d86fcf6f41ba11d391827ec3181155
Praveen <praying.sharma@gmail.com>
2025-04-10 14:55:23 +0530

    Add

--- Main.java
+++ Main.java
@@ -5,21 +5,22 @@ import java.util.Stack;
 public class Main {
 
     public static int power(int a, int b) {
-        if (a == 1) {
-            return b;
-
-        }
         if (b == 1) {
             return a;
         }
-        return a * power(a, b - 1);
-
+        if(a==1){
+            return b;
+        }
+        int result=power(a, b/2);
+        return result*result;
     }
+    
+    
 
     public static void main(String[] args) {
 
         int a = 2;
-        int b = 3;
+        int b = 4;
         System.out.println(power(a, b));
 
     }
