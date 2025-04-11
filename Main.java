5fb20c0a8a993a5dac661cc71d166f5f1e132c0b
Praveen <praying.sharma@gmail.com>
2025-04-11 17:05:44 +0530

    Add

--- Main.java
+++ Main.java
@@ -3,19 +3,7 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class Main {
-    // given a number num and a value k, print k multiples of num;
-    public static void multiples(int num, int k) {
-        // base case
-        if (k == 1) {
-            System.out.print(num * k + " ");
-            return;
-
-        }
-        multiples(num, k - 1);
-        int finaal = num * k;
-        System.out.print(finaal + " ");
-
-    }
+   
 
     public static void main(String[] args) {
 
