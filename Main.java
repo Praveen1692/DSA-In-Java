7457ac858e2185fb139720e0e41c7bb817aaee46
Praveen <praying.sharma@gmail.com>
2025-04-15 21:01:14 +0530

    Add

--- Main.java
+++ Main.java
@@ -4,54 +4,13 @@ import java.util.Stack;
 
 public class Main {
 
-    static class LLNode {
-        int data;
-        LLNode next;
-
-        public LLNode(int d) {
-            data = d;
-            next = null;
-        }
-    }
-
-    static void displayLL(LLNode head){
-        LLNode temp=head;
-        while (temp!=null) 
-        {
-            System.out.print(temp.data+" ");
-            temp=temp.next;
-            
-        }
-    }
-
-    static int countNode(LLNode head){
-        LLNode temp=head;
-        int count=0;
-        while (temp!=null) {
-            count++;
-            temp=temp.next;
-            
-        }
-        return count;
-    }
+   
 
     public static void main(String[] args) {
 
-        LLNode l1 = new LLNode(100);
-        LLNode l2 = new LLNode(200);
-        LLNode l3 = new LLNode(400);
-        LLNode l4 = new LLNode(500);
-        LLNode l5 = new LLNode(300);
+        System.out.println("Hello World");
 
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-
-        System.out.println("SLL is created");
-        displayLL(l1);
-        int count=countNode(l1);
-        System.out.println(count);
+      
 
 
     }
