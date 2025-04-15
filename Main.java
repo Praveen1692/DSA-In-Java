94bf8bbafc86cc08dfa3f6eb9c68f55fc5a6aa61
Praveen <praying.sharma@gmail.com>
2025-04-15 21:35:35 +0530

    Add

--- Main.java
+++ Main.java
@@ -4,11 +4,28 @@ import java.util.Stack;
 
 public class Main {
 
+    static boolean isPresent(int[] arr,int idx,int x){
+        if(idx>=arr.length){
+            return false;
+
+        }
+        if(arr[idx]==x){
+            return true;
+        }
+        return isPresent(arr, idx+1, x);
+
+    }
+
    
 
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        int[] arr={1,2,4,7,9,10,5,0,60};
+        int idx=0;
+        int x=600;
+        System.out.println(isPresent(arr, idx, x));
 
       
 
