28c8541e4a878b19d8a7cd63ace7558c6b424ce0
Praveen <praying.sharma@gmail.com>
2025-04-16 15:34:54 +0530

    Add

--- Main.java
+++ Main.java
@@ -5,13 +5,22 @@ import java.util.Stack;
 
 public class Main {
 
-  
-
+    static String Remove(String str, int idx, int n) {
+        if (idx >= n) {
+            return "";
+        }
+        if (str.charAt(idx) == 'a') {
+            return Remove(str, idx + 1, n);
+        }
+        return str.charAt(idx) + Remove(str, idx + 1, n);
+    }
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-
-        
+        String str = "abacax";
+        int n = str.length();
+        int idx = 0;
+        System.out.println(Remove(str, idx, n));
 
     }
 }
