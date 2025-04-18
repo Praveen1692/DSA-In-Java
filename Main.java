3e9b77574cccd47b9b97dc6362a537dd2d860385
Praveen <praying.sharma@gmail.com>
2025-04-18 22:43:37 +0530

    Add

--- Main.java
+++ Main.java
@@ -9,10 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String str = "ABC";
-        int l = 0;
-        int r = str.length() - 1;
-
+       
         
 
     }
