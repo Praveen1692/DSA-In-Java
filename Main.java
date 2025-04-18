8924ce6de8edc8175914e9290dfc9cdc81b205f0
Praveen <praying.sharma@gmail.com>
2025-04-18 16:43:07 +0530

    Add

--- Main.java
+++ Main.java
@@ -5,29 +5,7 @@ import java.util.Stack;
 
 public class Main {
 
-    static void permute(String str, int l, int r) {
-        if (l == r) {
-            System.out.print(str + " ");
-        } else {
-            for (int i = l; i <= r; i++) {
-                str = swap(str, l, i);
-                permute(str, l + 1, r);
-
-                // backtring;
-                str = swap(str, l, i);
-            }
-        }
-
-    }
-
-    static String swap(String str, int i, int j) {
-        char[] charArray = str.toCharArray();
-        char temp = charArray[i];
-        charArray[i] = charArray[j];
-        charArray[j] = temp;
-
-        return String.valueOf(charArray);
-    }
+    
 
     public static void main(String[] args) {
 
@@ -35,7 +13,7 @@ public class Main {
         int l = 0;
         int r = str.length() - 1;
 
-        permute(str, l, r);
+        
 
     }
 }
