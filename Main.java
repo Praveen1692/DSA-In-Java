8b374e1f9fde1a4828c6622115fd443b92207766
Praveen <praying.sharma@gmail.com>
2025-04-20 16:00:28 +0530

    Add

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     static void swap(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
                 if (arr[j] == 0 && arr[j + 1] != 0) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
