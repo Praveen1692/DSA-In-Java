8b0b78b67ac720a5c8dcd2e42723aa64b1c11ed0
Praveen <praying.sharma@gmail.com>
2025-04-22 13:44:07 +0530

    Add

--- Main.java
+++ Main.java
@@ -5,28 +5,33 @@ import java.util.Stack;
 
 public class Main {
 
-    static int findMax(int[] arr){
-        int mx=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>mx){
-                mx=arr[i];
+    static void sort(int[] arr) {
+        int n = arr.length;
+        int x = -1, y = -1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+                if (x == -1) {
+                    x = i - 1;
+                    y = i;
+                } else {
+                    y = i;
+                }
             }
         }
-        return mx;
 
-    }
-
-    static void countSort(int[] arr) {
-
-        int max=findMax(arr);
+        // swap two number;
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
 
     }
 
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        int[] arr = { 10, 50, 90, 70, 60, 40 };
-        countSort(arr);
+        int[] arr = { 10,5,6,7,8,9,3 };
+        sort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
