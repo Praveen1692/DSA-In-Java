9761c56fb8ab9682e1282f391619044a12723c4c
Praveen <praying.sharma@gmail.com>
2025-04-23 15:39:43 +0530

    Add

--- Main.java
+++ Main.java
@@ -1,38 +1,46 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Stack;
-
 public class Main {
 
-    static void sort(int[] arr) {
-        int n = arr.length;
-        int x = -1, y = -1;
+    static Node head = null;
 
-        for (int i = 1; i < n; i++) {
-            if (arr[i - 1] > arr[i]) {
-                if (x == -1) {
-                    x = i - 1;
-                    y = i;
-                } else {
-                    y = i;
-                }
-            }
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
         }
+    }
 
-        // swap two number;
-        int temp = arr[x];
-        arr[x] = arr[y];
-        arr[y] = temp;
+    static void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
 
+    static void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
     }
 
     public static void main(String[] args) {
-
         System.out.println("Hello");
-        int[] arr = { 10,5,6,7,8,9,3 };
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
 
+        insertAtEnd(100);
+        insertAtEnd(200);
+        insertAtEnd(300);
+
+        print(); // Output: 100 200 300
     }
 }
