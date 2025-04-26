3f10ec85cf4cd0a170ff8bfce894ef8a166a3bc8
Praveen <praying.sharma@gmail.com>
2025-04-26 13:22:15 +0530

    Add

--- Main.java
+++ Main.java
@@ -1,72 +1,18 @@
 public class Main {
 
-    static Node head = null;
-
-    static class Node {
-        int data;
-        Node next;
-
-        Node(int d) {
-            data = d;
-            next = null;
+    static int searchInRotated(int[] arr){
+        int ans=Integer.MIN_VALUE;
+        while () {
+            
         }
     }
 
-    static void print() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-    }
-
-    static void insertAtEnd(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        } else {
-            Node temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-        }
-    }
-
-    static void deleteAtPos(int pos) {
-        Node temp = head;
-        if (temp == null) {
-            return;
-        }
-        if (pos == 0) {
-            head = temp.next;
-            return;
-
-        }
-
-        for (int i = 0; temp != null && i < pos - 1; i++) {
-            temp = temp.next;
-        }
-        if (temp == null && temp.next != null) {
-            return;
-        }
-        temp.next = temp.next.next;
-
-    }
+    
 
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        insertAtEnd(100);
-        insertAtEnd(200);
-        insertAtEnd(300);
-        insertAtEnd(100);
-        insertAtEnd(200);
-        insertAtEnd(300);
-        deleteAtPos(2);
-
-        print(); // Output: 100 200 300
+       
         System.out.println("Hello World");
     }
 }
