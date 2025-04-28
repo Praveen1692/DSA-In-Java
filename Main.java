916771be34231846e12bdba549552e88fceb85fe
Praveen <praying.sharma@gmail.com>
2025-04-28 21:52:04 +0530

    Add

--- Main.java
+++ Main.java
@@ -1,31 +1,19 @@
 public class Main {
 
-    static int searchInRotated(int[] arr){
-        int ans=Integer.MIN_VALUE;
-        int n=arr.length;
-        int start=0;
-
-        int end=n-1;
-        while (start<=end) {
-            int mid=start+(end-start)/2;
-
-            if(arr[mid]<arr[n-1]){
-                ans=mid;
-                end=mid-1;
-            }else{
-                start=mid+1;
+    static void subString(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            String substr = "";
+            for (int j = i; j < str.length(); j++) {
+                substr += str.charAt(j);
+                System.out.print(substr + " ");
             }
-
-            
         }
-        return ans;
+        System.out.println();
     }
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        int[] arr = { 3, 4, 5, 1, 2 };
-        System.out.println(searchInRotated(arr));
-
-        System.out.println("Hello World");
+        String str = "abcd";
+        subString(str);
     }
 }
