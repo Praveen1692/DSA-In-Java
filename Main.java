ff3793673bfd7c5d44f6c7d0197bca3561d6b770
Praveen <praying.sharma@gmail.com>
2025-04-30 11:45:01 +0530

    Add

--- Main.java
+++ Main.java
@@ -2,19 +2,38 @@ public class Main {
     static int searchInRotatedArray(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-        while (start < end) {
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                return ans;
+            } else if (arr[mid] < arr[end]) { // mid to end sorted
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+
+                }
+            } else {// start to mid is sorted;
+                if (target > arr[start] && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+
+            }
 
         }
 
-
-        return -1;
+        return ans;
 
     }
 
     public static void main(String[] args) {
         System.out.println("Hello");
         int[] arr = { 3, 4, 5, 1, 2 };
-        int target = 4;
-        System.out.println(searchInRotatedArray(arr,target));
+        int target = 1;
+        System.out.println(searchInRotatedArray(arr, target));
     }
 }
