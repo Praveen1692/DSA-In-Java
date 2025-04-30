d6eeee0fd00af6acc3baeea23cc3cec71d76a1a5
Praveen <praying.sharma@gmail.com>
2025-04-30 14:15:22 +0530

    Add

--- Main.java
+++ Main.java
@@ -1,39 +1,34 @@
 public class Main {
-    static int searchInRotatedArray(int[] arr, int target) {
+
+    static boolean searchIn2DMatrix(int[][] arr, int target) {
+        // number of rows=n, number of cols=m;
+        int n = arr.length;
+        int m = arr[0].length;
         int start = 0;
-        int end = arr.length - 1;
-        int ans = -1;
+        int end = m * n - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
-                ans = mid;
-                return ans;
-            } else if (arr[mid] < arr[end]) { // mid to end sorted
-                if (target > arr[mid] && target <= arr[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
+            int midElement = arr[mid / m][mid % n];
 
-                }
-            } else {// start to mid is sorted;
-                if (target > arr[start] && target < arr[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-
+            if (midElement == target) {
+                return true;
+            }
+            if (target < midElement) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
 
         }
-
-        return ans;
+        return false;
 
     }
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        int[] arr = { 3, 4, 5, 1, 2 };
-        int target = 1;
-        System.out.println(searchInRotatedArray(arr, target));
+        int[][] arr = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+        int target = 106;
+        System.out.println(searchIn2DMatrix(arr, target));
+
     }
 }
