f157f68e585f5b16b6471fbd0ab13be6240624ce
Praveen <praying.sharma@gmail.com>
2025-04-30 14:16:15 +0530

    Add

--- Main.java
+++ Main.java
@@ -1,28 +1,6 @@
 public class Main {
 
-    static boolean searchIn2DMatrix(int[][] arr, int target) {
-        // number of rows=n, number of cols=m;
-        int n = arr.length;
-        int m = arr[0].length;
-        int start = 0;
-        int end = m * n - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            int midElement = arr[mid / m][mid % n];
-
-            if (midElement == target) {
-                return true;
-            }
-            if (target < midElement) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-
-        }
-        return false;
-
-    }
+   
 
     public static void main(String[] args) {
         System.out.println("Hello");
